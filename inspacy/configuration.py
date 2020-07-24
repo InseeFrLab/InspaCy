77ba613856f2ef6cb8438329fe26e8fab61dd8bd
dzikowski <dzkwsk@gmail.com>
2020-07-24 14:56:59 +0000

    clean test and config modification

--- inspacy/configuration.py
+++ inspacy/configuration.py
@@ -4,7 +4,7 @@ import configparser
 relativeLocation = "/inspacy/static/config.ini"
 
 cfg = configparser.ConfigParser()
-if os.environ.get('INSPACY_HOME') is not None:
-    cfg.read(os.environ['INSPACY_HOME'] + relativeLocation)
-else:
-    cfg.read("./static/config.ini")
+if os.environ.get('INSPACY_HOME') is None:
+    os.environ['INSPACY_HOME'] = '.'
+
+cfg.read(os.environ['INSPACY_HOME'] + relativeLocation)
